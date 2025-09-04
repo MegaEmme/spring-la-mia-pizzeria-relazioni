4681de2656b2aea66b6848a0d439b9cee46abff6
MegaEmme <mikmusco@gmail.com>
2025-09-04 14:36:09 +0200

    feat: add ingredients to pizzas

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.SpecialOffer;
+import org.lessons.java.spring_la_mia_pizzeria_crud.repository.IngredientRepository;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.SpecialOfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,9 @@ public class PizzaController {
 
     @Autowired
     private SpecialOfferRepository offerRepository;
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
 
     // INDEX
     @GetMapping
@@ -58,6 +62,7 @@ public class PizzaController {
     @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("pizza", new Pizza());
+        model.addAttribute("ingredients", ingredientRepository.findAll());
         return "pizzas/create";
     }
 
@@ -65,6 +70,7 @@ public class PizzaController {
     public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
 
         if (bindingResult.hasErrors()) {
+            model.addAttribute("ingredients", ingredientRepository.findAll());
             return "pizzas/create";
         }
 
@@ -77,6 +83,7 @@ public class PizzaController {
     @GetMapping("/edit/{id}")
     public String edit(@PathVariable Integer id, Model model) {
         model.addAttribute("pizza", repository.findById(id).get());
+        model.addAttribute("ingredients", ingredientRepository.findAll());
         return "pizzas/edit";
     }
 
@@ -84,6 +91,7 @@ public class PizzaController {
     public String update(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
 
         if (bindingResult.hasErrors()) {
+            model.addAttribute("ingredients", ingredientRepository.findAll());
             return "pizzas/edit";
         }
 
